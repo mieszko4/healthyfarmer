3860c757bebce4cfbe39ae9bf32667a6f0a49b61
mieszko4 <mieszko4@gmail.com>
2021-05-29 13:30:12 +0200

    Connect video

--- app/src/Video/index.tsx
+++ app/src/Video/index.tsx
@@ -1,22 +1,42 @@
 import * as tf from '@tensorflow/tfjs';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const loadVideoModel = async () => {
+const load = async () => {
   const model = await tf.loadGraphModel('./models/health/model.json');
+  
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: { facingMode: 'environment' }
+  });
 
-  return model;
+  return { model, stream };
 }
 
 export const Video = () => {
+  const [model, setModel] = useState<tf.GraphModel | null>(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
   useEffect(() => {
-    loadVideoModel();
+    load().then(({ model, stream }) => {
+      setModel(model);
+      setStream(stream);
+    });
   }, []);
+
+  useEffect(() => {
+    if (stream && videoRef.current) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream])
   
   return (
     <>
       <p>Make a video of leaves - when unhealthy leave is detected you will be notified.</p>
 
-      
+      {stream && (
+        <video ref={videoRef} autoPlay />
+      )}
     </>
   );
 };
