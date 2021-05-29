68aff721fa8fc028097a64277ec9296b662e3ebe
mieszko4 <mieszko4@gmail.com>
2021-05-29 13:17:11 +0200

    Connect tensor with health model

--- app/src/Video/index.tsx
+++ app/src/Video/index.tsx
@@ -1,5 +1,22 @@
-export const Video = () => (
-  <>
-    <p>Let's start a video!</p>
-  </>
-)
+import * as tf from '@tensorflow/tfjs';
+import { useEffect } from 'react';
+
+const loadVideoModel = async () => {
+  const model = await tf.loadGraphModel('./models/health/model.json');
+
+  return model;
+}
+
+export const Video = () => {
+  useEffect(() => {
+    loadVideoModel();
+  }, []);
+  
+  return (
+    <>
+      <p>Make a video of leaves - when unhealthy leave is detected you will be notified.</p>
+
+      
+    </>
+  );
+};
